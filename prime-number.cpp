cbd33cfddb8de65fbb3750acfd420aa7bd29d477
Tanvir Hasan Serazy <amihasan420@gmail.com>
2022-09-10 03:01:44 +0600

    Second

--- prime-number.cpp
+++ prime-number.cpp
@@ -3,8 +3,38 @@ using namespace std;
 
 int main(){
 
+    int number, count = 0;
+
+    cout<<"\nEnter your number"<<endl;
+    cin>>number;
+
+    for (int i = 1; i <= number; i++)
+    {
+        if (number%i == 0)
+        {
+            count++;
+        }
+        
+    }
+        
+    if (count == 2)
+    {
+        cout<<"It is a prime number"<<endl;
+    }
+
+    else
+    {
+
+        cout<<"It is not a prime number"<<endl;
+
+    }
+           
+
+
 
     
+    
+
 
 
 
